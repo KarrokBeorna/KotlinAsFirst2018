d6bcf6acd9f87bf44afdac526fecbb39729a12f3
Dmitriy <savelev_lotr@mail.ru>
2018-09-26 23:15:31 +0300

    Окончательно исправлен 2 урок (IfElse)

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -64,8 +64,9 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
 fun ageDescription(age: Int): String = when {
-    (age % 10 == 1) && (age != 11) && (age != 111) -> "$age год"
-    (age % 10 > 1) && (age % 10 < 5) && (age != 12) && (age != 13) && (age != 14) && (age != 112) && (age != 113) && (age != 114) -> "$age года"
+    (age in 11..14) || (age in 111..114) -> "$age лет"
+    (age % 10 == 1) -> "$age год"
+    (age % 10 > 1) && (age % 10 < 5) -> "$age года"
     else -> "$age лет"
 }
 
@@ -82,9 +83,9 @@ fun timeForHalfWay(t1: Double, v1: Double,
     val s = t1 * v1 + t2 * v2 + t3 * v3
     val sred = s / 2.0
     when {
-     (t1 * v1 > sred) -> {val time = sred / v1; return time}
-     (t1 * v1 + t2 * v2 < sred) -> {val time = (sred - (t1 * v1 + t2 * v2)) / v3 + t1 + t2; return time}
-    else -> {val time = (sred - t1 * v1) / v2 + t1; return time}
+        (t1 * v1 > sred) -> return sred / v1
+        (t1 * v1 + t2 * v2 < sred) -> return (sred - (t1 * v1 + t2 * v2)) / v3 + t1 + t2
+        else -> return (sred - t1 * v1) / v2 + t1
 }}
 
 
@@ -100,12 +101,11 @@ fun timeForHalfWay(t1: Double, v1: Double,
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
                        rookX2: Int, rookY2: Int): Int {
-   val t = 0
     when{
-        (((kingX == rookX1) && (kingY == rookY2)) || ((kingY == rookY1) && (kingX == rookX2))) -> return t + 3
-        ((kingX == rookX1) || (kingY == rookY1)) -> return t + 1
-        ((kingX == rookX2) || (kingY == rookY2)) -> return t + 2
-        else -> return t
+        (((kingX == rookX1) && (kingY == rookY2)) || ((kingY == rookY1) && (kingX == rookX2))) -> return 3
+        ((kingX == rookX1) || (kingY == rookY1)) -> return 1
+        ((kingX == rookX2) || (kingY == rookY2)) -> return 2
+        else -> return 0
 }}
 
 
@@ -122,12 +122,11 @@ fun whichRookThreatens(kingX: Int, kingY: Int,
 fun rookOrBishopThreatens(kingX: Int, kingY: Int,
                           rookX: Int, rookY: Int,
                           bishopX: Int, bishopY: Int): Int {
-    val t = 0
     when {
-        (((kingX == rookX) || (kingY == rookY)) && (abs(kingX - bishopX) == abs(kingY - bishopY))) -> return t + 3
-        ((kingX == rookX) || (kingY == rookY)) -> return t + 1
-        (abs(kingX - bishopX) == abs(kingY - bishopY)) -> return t + 2
-        else -> return t
+        (((kingX == rookX) || (kingY == rookY)) && (abs(kingX - bishopX) == abs(kingY - bishopY))) -> return 3
+        ((kingX == rookX) || (kingY == rookY)) -> return 1
+        (abs(kingX - bishopX) == abs(kingY - bishopY)) -> return 2
+        else -> return 0
     }
 }
 
@@ -140,12 +139,11 @@ fun rookOrBishopThreatens(kingX: Int, kingY: Int,
  * Если такой треугольник не существует, вернуть -1.
  */
 fun triangleKind(a: Double, b: Double, c: Double): Int {
-    val t = 0
     when {
-        (a + b < c) || (a + c < b) || (b + c < a) -> return t - 1
-        (a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a) -> return t + 1
-        (a * a + b * b > c * c) && (a * a + c * c > b * b) && (b * b + c * c > a * a) -> return t
-        else -> return t + 2
+        (a + b < c) || (a + c < b) || (b + c < a) -> return -1
+        (a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a) -> return 1
+        (a * a + b * b > c * c) && (a * a + c * c > b * b) && (b * b + c * c > a * a) -> return 0
+        else -> return 2
     }
 }
 
@@ -158,12 +156,11 @@ fun triangleKind(a: Double, b: Double, c: Double): Int {
  * Если пересечения нет, вернуть -1.
  */
 fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
-    val t = 0
     when {
-        (c <= a && d >= a && d <= b) -> return d - a
+        (a in c..d && d <= b) -> return d - a
         (c <= a && d >= b) -> return b - a
         (c >= a && d <= b) -> return d - c
-        (c >= a && c <= b && d >= b) -> return b - c
-        else -> return t - 1
+        (c in a..b && d >= b) -> return b - c
+        else -> return -1
     }
 }
