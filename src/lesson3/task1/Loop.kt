844c00b763f8482a4294167843b32ab312331529
Dmitriy <savelev_lotr@mail.ru>
2018-10-01 22:00:08 +0300

    Исправлен sin

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -208,11 +208,13 @@ fun sin(x: Double, eps: Double): Double {
     else {
         do {
             for (i in 1..Int.MAX_VALUE step 4) {
-                z = (pow(x, i.toDouble()) / factorial); unit += z
-                if (z < eps) break
+                z = (pow(x, i.toDouble()) / factorial)
+                if (abs(z) < eps) break
+                unit += z
                 factorial *= (i + 1) * (i + 2)
-                z = (pow(x, (i + 2).toDouble()) / factorial); unit -= z
-                if (z < eps) break
+                z = (pow(x, (i + 2).toDouble()) / factorial)
+                if (abs(z) < eps) break
+                unit -= z
                 factorial *= (i + 3) * (i + 4)
             }
         } while (z > eps)
@@ -237,16 +239,18 @@ fun cos(x: Double, eps: Double): Double {
     else {
         do {
             for (i in 0..Int.MAX_VALUE step 4) {
-                z = (pow(x, i.toDouble()) / factorial); unit += z
-                if (z < eps) break
+                z = (pow(x, i.toDouble()) / factorial)
+                if (abs(z) <= eps) break
+                unit += z
                 factorial *= (i + 1) * (i + 2)
-                z = (pow(x, (i + 2).toDouble()) / factorial); unit -= z
-                if (z < eps) break
+                z = (pow(x, (i + 2).toDouble()) / factorial)
+                unit -= z
                 factorial *= (i + 3) * (i + 4)
+                if (abs(z) <= eps) break
             }
-        } while (z > eps)
+        } while (abs(z) > eps)
     }
-    return unit
+    return abs(unit)
 }
 
 /**
